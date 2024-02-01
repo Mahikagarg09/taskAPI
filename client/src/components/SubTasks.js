8ba3c2c707873bdb0bbbf3f4fe8a9b909e6872c3
Mahika Garg <mahikagarg23@gmail.com>
2024-02-01 23:11:32 +0530

    update subtask

--- client/src/components/SubTasks.js
+++ client/src/components/SubTasks.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 
 const SubTasks = ({ taskId }) => {
     const [subtasks, setSubtasks] = useState([]);
+    const [editablesubTaskId, setEditablesubTaskId] = useState(null);
+    const [originalsubTasks, setOriginalsubTasks] = useState([]);
 
     useEffect(() => {
         const fetchSubtasks = async () => {
@@ -12,8 +14,8 @@ const SubTasks = ({ taskId }) => {
 
                 const response = await axios.get(`http://localhost:3000/api/subtasks/${user_id}/subtasks`, { params: { task_id } });
                 const data = response.data;
-                console.log(data)
                 setSubtasks(data);
+                setOriginalsubTasks(data);
             } catch (error) {
                 console.error(error);
             }
@@ -21,6 +23,57 @@ const SubTasks = ({ taskId }) => {
 
         fetchSubtasks();
     }, [taskId]);
+
+    const handleEditClick = (subtaskId) => {
+        setEditablesubTaskId(subtaskId)
+    };
+
+    const handleCancelClick = () => {
+        setEditablesubTaskId(null);
+        setSubtasks(originalsubTasks)
+    };
+
+
+    const handleUpdateClick = async (subtaskId, newStatus) => {
+        try {
+            const response = await axios.put(`http://localhost:3000/api/subtasks/${subtaskId}`, {
+                status: newStatus,
+            });
+
+            // Update the local state with the updated task
+            setSubtasks((prevTasks) =>
+                prevTasks.map((subtask) => (subtask._id === subtaskId ? response.data : subtask))
+            );
+
+            setEditablesubTaskId(null); // Move this line inside the try block
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleStatusChange = (subtaskId, newStatus) => {
+        setSubtasks((prevsubTasks) =>
+            prevsubTasks.map((subtask) =>
+                subtask._id === subtaskId ? { ...subtask, status: newStatus } : subtask
+            )
+        );
+    };
+
+    // const handleDeleteClick = async (taskId) => {
+    //     try {
+    //         // Make a DELETE request to delete the task on the server
+    //         await axios.delete(`http://localhost:3000/api/tasks/${taskId}`);
+
+    //         // Update the local state to remove the deleted task
+    //         setTasks((prevTasks) =>
+    //             prevTasks.filter((task) => task._id !== taskId)
+    //         );
+
+    //         setEditableTaskId(null); // Reset the editable task ID
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // };
 
 
     return (
@@ -36,22 +89,54 @@ const SubTasks = ({ taskId }) => {
                 </thead>
                 <tbody>
                     {subtasks.map((sub) => (
-                        <tr key={sub._id}>
+                        <tr key={sub._id} className={editablesubTaskId === sub._id ? "bg-gray-200" : "hover:bg-gray-100"}>
                             <td className="py-2 px-4 border-b text-center">{sub.title}</td>
-                            <td className="py-2 px-4 border-b text-center">{sub.status}</td>
                             <td className="py-2 px-4 border-b text-center">
-                                <button
-                                    // onClick={() => handleEditClick(task._id)}
-                                    className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300 mr-2"
+                            {editablesubTaskId === sub._id ? (
+                                <select
+                                    value={sub.status}
+                                    onChange={(e) => handleStatusChange(sub._id, e.target.value)}
+                                    className="border border-gray-300 px-2 py-1 rounded-md"
                                 >
-                                    Update
-                                </button>
-                                <button
-                                    // onClick={() => handleDeleteClick(task._id)}
-                                    className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 focus:outline-none focus:ring focus:border-red-300 mr-2"
-                                >
-                                    Delete
-                                </button>
+                                    <option value="0">0-incomplete</option>
+                                    <option value="1">1-complete</option>
+                                </select>
+                            ) : (
+                                sub.status
+                            )}
+                            </td>
+                            <td className="py-2 px-4 border-b text-center">
+                                {editablesubTaskId=== sub._id ? (
+                                    <>
+                                        <button
+                                            onClick={() => handleUpdateClick(sub._id, sub.status)}
+                                            className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 focus:outline-none focus:ring focus:border-green-300 mr-2"
+                                        >
+                                            Done
+                                        </button>
+                                        <button
+                                            onClick={handleCancelClick}
+                                            className="bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600 focus:outline-none focus:ring focus:border-gray-300"
+                                        >
+                                            Cancel
+                                        </button>
+                                    </>
+                                ) : (
+                                    <>
+                                        <button
+                                            onClick={() => handleEditClick(sub._id)}
+                                            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300 mr-2"
+                                        >
+                                            Update
+                                        </button>
+                                        <button
+                                            // onClick={() => handleDeleteClick(task._id)}
+                                            className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 focus:outline-none focus:ring focus:border-red-300 mr-2"
+                                        >
+                                            Delete
+                                        </button>
+                                    </>
+                                )}
                             </td>
                         </tr>
                     ))}
