f0e0dd9fe85e68b609404cf64a62a0198f802724
Mahika-gwd <mg@gwd.collectve.com>
2024-02-01 13:48:01 +0530

    react-route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import Auth from './components/Auth'
 import './index.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Tasks from './components/Tasks';
 
 export default function App() {
   return (
     <div>
-      <Auth/>
+      <Router>
+        <Routes>
+        <Route path="/" element={<Tasks/>} />
+          <Route path="/auth" element={<Auth/>} />
+        </Routes>
+      </Router>
     </div>
   )
 }
